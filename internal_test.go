6c403f6d7264f6cb8ee82777397b25c09f28212c
Sonia <soniakeys@gmail.com>
2017-12-17 07:07:35 -0500

    fix printfs

--- internal_test.go
+++ internal_test.go
@@ -13,7 +13,7 @@ func TestLimbWeight(t *testing.T) {
 	}
 	min, i, k := d.limbWeight(1)
 	if min != 2 || i != 2 || k != 0 {
-		t.Fatalf("got %d %d %d, want 2 2 0", min, i, k)
+		t.Fatalf("got %f %d %d, want 2 2 0", min, i, k)
 	}
 }
 
@@ -26,10 +26,10 @@ func TestLimbWeightSubMatrix(t *testing.T) {
 	}
 	min, i, k := d.limbWeightSubMatrix(3)
 	if min != 7 || i != 1 || k != 2 {
-		t.Fatalf("got %d %d %d, want 7 1 2", min, i, k)
+		t.Fatalf("got %f %d %d, want 7 1 2", min, i, k)
 	}
 	min, i, k = d.limbWeightSubMatrix(2)
 	if min != 10 || i != 0 || k != 1 {
-		t.Fatalf("got %d %d %d, want 10 0 1", min, i, k)
+		t.Fatalf("got %f %d %d, want 10 0 1", min, i, k)
 	}
 }
